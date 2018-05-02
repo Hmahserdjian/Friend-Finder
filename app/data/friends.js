434a42fa82c8c81e4940e5b39329f15896e691c2
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-05-02 19:26:47 -0400

    updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,4 @@
 
-    // Question: What does this code do?
-    $("#add-btn").on("click", function(event) {
-        event.preventDefault();
-    var answer = {
-         name: $("#name").val().trim(),
-         role: $("#role").val().trim(),
-         age: $("#age").val().trim(),
-         forcePoints: $("#force-points").val().trim()
-  };
 
   // Question: What does this code do??
   $.post("/api/html", answer)
@@ -15,4 +6,31 @@
         console.log("add.html", data);
     alert("Adding character...");
   });
+
+
+$("#add-btn").on("click", function (event) {
+    event.preventDefault();
+    var dropdownOne = {
+        1: $("#Q1").val(),
+        2: $("#Q2").val(),
+        3: $("#Q3").val(),
+        4: $("#Q4").val(),
+        5: $("#Q5").val(),
+        6: $("#Q6").val(),
+        7: $("#Q7").val(),
+        8: $("#Q8").val(),
+        9: $("#Q9").val(),
+        10: $("#Q10").val()
+    };
+
+    $.post("/api/html", answer)
+        .then(function(data){
+            console.log("add.html", data);
+        alert("adding answer...");
+        });
 });
+
+    $("#survey").on("click",  function (event){
+        event.preventDefault();
+        location.href = "/survey"
+})
--- server.js
+++ server.js
@@ -2,3 +2,55 @@ var http = require("http");
 var express = require("express")
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var question = [
+    {
+        routeQuestion: "Do you like cheese?"
+    },
+    {
+        routeQuestion: "Do you like puppies?"
+    },
+    {
+        routeQuestion: "Do you like coding?"
+    },
+    {
+        routeQuestion: "Do you like movies?"
+    },
+    {
+        routeQuestion: "Do you like facesuckers from aliens?"
+    },
+    {
+        routeQuestion: "Is Thanos the greatest hero of all time?"
+    },
+    {
+        routeQuestion: "Do you like Call of Duty?"
+    },
+    {
+        routeQuestion: "Do you like Samsung?"
+    },
+    {
+        routeQuestion: "Do you like the less superior apple?"
+    },
+    {
+        routeQuestion: ""
+    }
+];
+
+app.get("/home.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
